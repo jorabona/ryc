d78f5cddcff24e1851202f97b4508bfb1d8c885c
jorabona <126348022+jorabona@users.noreply.github.com>
2024-04-29 10:11:52 -0400

    removed E, corrected postion of V & I
    Automated exclude processing in XLSX

--- chart-gen/data.js
+++ chart-gen/data.js
@@ -6,13 +6,13 @@ let array = [
 {id: 1, mark: 'A',lat: 40.4866647222222,log: -74.2530380555555,descr: 'GREAT BEDS LIGHT'},
 {id: 2, mark: 'B',lat: 40.5012438888889,log: -74.2140522222222,descr: 'G 39 Can'},
 {id: 3, mark: 'D',lat: 40.4967536111111,log: -74.1334833333333,descr: 'Channel lighted R 14'},
-{id: 4, mark: 'E',lat: 40.5123066666667,log: -74.0987175,descr: 'OLD ORCHARD LIGHT'},
+//removed "E"
 {id: 5, mark: 'G',lat: 40.5379691666667,log: -74.0428663888889,descr: 'WEST BANK LIGHT'},
 {id: 6, mark: 'H',lat: 40.5130152777778,log: -74.0135238888889,descr: 'ROMER SHOAL LIGHT'},
-{id: 7, mark: 'I',lat: 40.6441072222222,log: -74.1560758333333,descr: 'G 19 FL G 4 Sec'},
+{id: 7, mark: 'I',lat: 40.5016167,log: -74.1623000,descr: 'G 19 FL G 4 Sec'},
 {id: 8, mark: 'J',lat: 40.4837266666667,log: -74.1257572222222,descr: 'G 11A FL G 4 Sec'},
 {id: 9, mark: 'K',lat: 40.5073091666667,log: -74.1958983333333,descr: 'G 31 FL G 2.5 Sec'},
-{id: 10, mark: 'L',lat: 40.4742266666667,log: -74.1834808333333,descr: 'KEYPORT 1'},
+{id: 10, mark: 'L',lat:     40.4905333,log: -74.2283333,descr: 'KEYPORT 1'},
 {id: 11, mark: 'M',lat: 0,log: 0,descr: 'MOBILE MARK'},
 {id: 12, mark: 'N',lat: 40.5063377777778,log: -74.1846241666667,descr: 'G 27 Qk FL'},
 {id: 13, mark: 'O',lat: 40.4890363888889,log: -74.1064802777778,descr: 'G 9 FL G 4 sec'},
@@ -24,11 +24,10 @@ let array = [
 {id: 17, mark: 'T',lat: 40.4838927777778,log: -74.2387877777778,descr: 'G Can 3'},
 {id: 18, mark: 'S/F',lat: 40.513015,log: -74.013524,descr: 'Start/Finish '},
 {id: 19, mark: 'U',lat: 40.4869483333333,log: -74.2341211111111,descr: 'G Can 1'},
-{id: 20, mark: 'V',lat: 40.490534,log: -74.2181216666667,descr: 'G 47 FL G 4 Sec'},
-{id: 21, mark: 'W',lat: 40.4844483333333,log: -74.2453158333333,descr: 'G Can 5 '},
+{id: 20, mark: 'V',lat: 40.490534,log: -74.22833333,descr: 'G 47 FL G 4 Sec'},
+{id: 21, mark: 'W',lat: 40.49053333,log: -74.2453158333333,descr: 'G Can 5 '},
 {id: 22, mark: 'X',lat: 40.4935313888889,log: -74.2659552777778,descr: 'G Can 5'},
 {id: 23, mark: 'Y',lat: 40.4900591666667,log: -74.2690944444444,descr: 'RG FL (2+1) R 6 sec'},
 {id: 24, mark: 'Z',lat: 40.4968925,log: -74.2628719444444,descr: 'G Can 3'}
 
     ];
-
